2402ce8cc697ae48e8a872f03b9252c349302f8b
archna94 <126779485+archna94@users.noreply.github.com>
2023-03-27 10:11:39 +0100

    updated

---
+++ main.tf
@@ -0,0 +1,110 @@
+
+resource "aws_kms_key" "this" {
+   count = var.create ? 1 : 0
+
+  deletion_window_in_days = var.deletion_window_in_days
+  policy = data.aws_iam_policy_document.this.json
+  tags = var.tags
+}
+
+
+resource "aws_kms_alias" "alias" {
+  target_key_id = aws_kms_key.this[0].key_id
+  name          = "alias/${var.environment}-${var.name}"
+}
+
+data "aws_iam_policy_document" "this" {
+
+  source_policy_documents   = var.source_policy_documents
+  override_policy_documents = var.override_policy_documents
+
+  # Default policy - account wide access to all key operations
+  dynamic "statement" {
+    for_each = var.enable_default_policy ? [1] : []
+
+    content {
+      sid       = "Default"
+      actions   = ["kms:*"]
+      resources = ["*"]
+
+      principals {
+        type        = "AWS"
+        identifiers = ["*"]
+      }
+    }
+  }
+
+  # Key owner - all key operations
+  dynamic "statement" {
+    for_each = length(var.key_owners) > 0 ? [1] : []
+
+    content {
+      sid       = "KeyOwner"
+      actions   = ["kms:*"]
+      resources = ["*"]
+
+      principals {
+        type        = "AWS"
+        identifiers = var.key_owners
+      }
+    }
+  }
+
+  # Key administrators - https://docs.aws.amazon.com/kms/latest/developerguide/key-policy-default.html#key-policy-default-allow-administrators
+  dynamic "statement" {
+    for_each = length(var.key_administrators) > 0 ? [1] : []
+
+    content {
+      sid = "KeyAdministration"
+      actions = [
+        "kms:Create*",
+        "kms:Describe*",
+        "kms:Enable*",
+        "kms:List*",
+        "kms:Put*",
+        "kms:Update*",
+        "kms:Revoke*",
+        "kms:Disable*",
+        "kms:Get*",
+        "kms:Delete*",
+        "kms:TagResource",
+        "kms:UntagResource",
+        "kms:ScheduleKeyDeletion",
+        "kms:CancelKeyDeletion",
+      ]
+      resources = ["*"]
+
+      principals {
+        type        = "AWS"
+        identifiers = var.key_administrators
+      }
+    }
+  }
+
+  # Key users - https://docs.aws.amazon.com/kms/latest/developerguide/key-policy-default.html#key-policy-default-allow-users
+  dynamic "statement" {
+    for_each = length(var.key_users) > 0 ? [1] : []
+
+    content {
+      sid = "KeyUsage"
+      actions = [
+        "kms:Encrypt",
+        "kms:Decrypt",
+        "kms:ReEncrypt*",
+        "kms:GenerateDataKey*",
+        "kms:DescribeKey",
+      ]
+      resources = ["*"]
+
+      principals {
+        type        = "AWS"
+        identifiers = var.key_users
+      }
+    }
+  }
+}
+
+
+
+
+  
